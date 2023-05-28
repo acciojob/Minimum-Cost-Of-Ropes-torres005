1716cc23f4bab5561b793e7b4bd7d3abf6b00b41
torres005 <109883423+torres005@users.noreply.github.com>
2023-05-28 21:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+
+const result = document.getElementById('result');
+const arr = document.getElementById('rope-length').split(',');
+	
+
+
+
+	
+	
 function calculateMinCost() {
-  //your code here
   
-  
-  
+  const minHeap = new PriorityQueue();
+	for(const len of arr){
+		minHeap.add(len);
+	}
+
+	let cost = 0;
+
+	while(minHeap.size() > 1) {
+		const min1 = minHeap.poll();
+		const min2 = minHeap.poll();
+
+		const sum = min1 + min2;
+
+		minHeap.add(sum);
+
+		cost += sum;
+	}
+	
+	result.innerText = cost;
 }  
+  
+  
+  
+ 
